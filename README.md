df50dfc64abe6c496ddc8b3e63bbea8e1b0f4434
Vishwas Magotra <vishwas251999@gmail.com>
2021-10-02 23:11:40 +0530

    multi screen app first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# multi_screen_utility_app
+
+A new Flutter project.
+
+Link to the [design](https://dribbble.com/shots/15027633-Multi-Tool-Utility-App)
---
+++ android/app/src/main/kotlin/com/example/multi_screen_utility_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multi_screen_utility_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

507c623288beb626df2b757ebaa435b092febad9
Vishwas Magotra <vishwas251999@gmail.com>
2021-10-05 00:02:20 +0530

    done till tools carousel

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,321 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List _gradientColor = [
+    [Colors.tealAccent, Colors.indigo.shade900],
+    [Colors.red.shade800, Colors.indigo.shade900],
+    [Colors.lightGreenAccent, Colors.indigo.shade900],
+    [Colors.orange.shade300, Colors.indigo.shade900],
+    [Colors.green, Colors.indigo.shade900]
+  ];
+
+  List _cardNames = [
+    'CONTACT\nBACKUP',
+    'MEDIA\nCLEANER',
+    'RECENT\nAPPS',
+    'FAVOURITE\nCONTACTS',
+    'POWER\nSAVER',
+  ];
+  List _icons = [
+    FaIcon(
+      FontAwesomeIcons.database,
+      color: Colors.teal.withOpacity(0.8),
+      size: 30,
+    ),
+    FaIcon(
+      FontAwesomeIcons.photoVideo,
+      color: Colors.teal.withOpacity(0.8),
+      size: 30,
+    ),
+    FaIcon(
+      FontAwesomeIcons.history,
+      color: Colors.teal.withOpacity(0.8),
+      size: 30,
+    ),
+    FaIcon(
+      FontAwesomeIcons.star,
+      color: Colors.teal.withOpacity(0.8),
+      size: 30,
+    ),
+    FaIcon(
+      FontAwesomeIcons.batteryEmpty,
+      color: Colors.teal.withOpacity(0.8),
+      size: 30,
+    ),
+  ];
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _key,
+      endDrawer: Drawer(
+        elevation: 0,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: Container(
+                decoration: BoxDecoration(color: Colors.black87),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text(
+                    'Application\nStatus',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        color: Colors.white54,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              tileColor: Colors.black87,
+              title: Text(
+                'Updated Apps',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              leading: FaIcon(
+                FontAwesomeIcons.check,
+                color: Colors.white38,
+              ),
+            ),
+            ListTile(
+              tileColor: Colors.black87,
+              title: Text(
+                'Update Pending',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              leading: FaIcon(
+                FontAwesomeIcons.exclamationCircle,
+                color: Colors.white38,
+              ),
+            ),
+            OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Update Apps',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                )),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/user1.jpg'),
+                  radius: 25,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'DASHBOARD\n',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 30,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                      TextSpan(
+                        text: '- PROTECTION LEVEL ',
+                        style: TextStyle(
+                          color: Color.fromRGBO(88, 93, 104, 1),
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w800,
+                          fontSize: 13,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'GOOD',
+                        style: TextStyle(
+                          color: Colors.orange[700],
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                IconButton(
+                  tooltip: 'Drawer',
+                  onPressed: () {
+                    _key.currentState!.openEndDrawer();
+                  },
+                  icon: FaIcon(
+                    FontAwesomeIcons.angleDoubleLeft,
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.box,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        'TOOLS',
+                        style: TextStyle(
+                          color: Colors.grey[350],
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'SLIDE RIGTH TO\n VIEWS TOOLS',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Color.fromRGBO(88, 93, 104, 1),
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w800,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            height: 255,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return Stack(
+                    alignment: Alignment.bottomLeft,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                        ),
+                        height: 250,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          gradient: LinearGradient(
+                              colors: _gradientColor[index],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                        ),
+                        height: 250,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 2,
+                              spreadRadius: 0,
+                              color: Colors.white.withOpacity(0.3),
+                            )
+                          ],
+                          color: Colors.transparent,
+                          border: Border.symmetric(
+                            horizontal: BorderSide(
+                              width: 1,
+                              color: Colors.transparent,
+                            ),
+                            vertical: BorderSide(
+                              width: 1,
+                              color: Colors.transparent,
+                            ),
+                          ),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50, bottom: 20),
+                        child: Text(
+                          _cardNames[index],
+                          maxLines: 2,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(left: 50, bottom: 155),
+                        height: 100,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.75),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(5),
+                            topRight: Radius.circular(5),
+                            bottomLeft: Radius.circular(15),
+                            bottomRight: Radius.circular(15),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 5,
+                              spreadRadius: 0,
+                              color: Colors.white.withOpacity(0.3),
+                            )
+                          ],
+                        ),
+                        child: _icons[index],
+                      ),
+                    ],
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
